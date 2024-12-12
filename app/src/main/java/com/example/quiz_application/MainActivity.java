66b5dc88e5d40f242ab376a57438f42020836d71
bouchrakhalfaouy <bkhalfaouy@gmail.com>
2024-12-12 13:37:32 +0100

    commit 2

--- app/src/main/java/com/example/quiz_application/MainActivity.java
+++ app/src/main/java/com/example/quiz_application/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.quiz_application;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Délai de 10 secondes avant de passer à l'activité suivante
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // Terminer l'activité Splash pour qu'elle ne soit pas réaffichée en arrière-plan
+        }, 10000); // 10 secondes
     }
 }
