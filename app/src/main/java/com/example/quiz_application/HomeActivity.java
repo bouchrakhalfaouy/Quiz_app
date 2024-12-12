bb996de56061dde35ea75c70edf0af6e083abfeb
bouchrakhalfaouy <bkhalfaouy@gmail.com>
2024-12-12 22:37:32 +0100

    commit 2

--- app/src/main/java/com/example/quiz_application/HomeActivity.java
+++ app/src/main/java/com/example/quiz_application/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.quiz_application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -66,4 +67,5 @@ public class HomeActivity extends AppCompatActivity {
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
+
 }
